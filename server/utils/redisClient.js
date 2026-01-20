ac9f0f35d4c51fcb376bf25b33928215386f9a05
Aadhi Keerthanan <404error@Aadhis-MacBook-Pro.local>
2026-01-20 12:26:29 +0530

    redis config changed

--- server/utils/redisClient.js
+++ server/utils/redisClient.js
@@ -2,7 +2,28 @@ import { createClient } from "redis";
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
 });
-redisClient.connect().catch(console.error);
+
+redisClient.on("connect", () => {
+  console.log("Redis connecting");
+});
+
+redisClient.on("ready", () => {
+  console.log("Redis ready");
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+redisClient.on("end", () => {
+  console.log("Redis disconnected");
+});
+
+await redisClient.connect();
 
 export { redisClient };
