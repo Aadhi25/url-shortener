45ba6ade3bc78b47675625e255f05a62814d7688
Aadhi Keerthanan <404error@Aadhis-MacBook-Pro.local>
2026-01-20 11:49:14 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -25,6 +25,8 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
